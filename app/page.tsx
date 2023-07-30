06997907fed683d3f83b9fa1a3d7af3dcb7988d1
Do Duy Anh <duyanhvn1999@gmail.com>
2023-07-30 15:20:13 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        Welcome to {process.env.NEXT_PUBLIC_APP_NAME}
+        Welcome to {process.env.NEXT_PUBLIC_LOOKUP_KEY}
       </h1>
     </main>
   );
